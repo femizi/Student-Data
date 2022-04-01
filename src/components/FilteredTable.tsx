d747198fdf42ebb6a495a9d0343c63c1602ffc02
femizi <obito.femi@gmail.com>
2022-04-01 11:56:41 +0100

    filtered responsive

--- src/components/FilteredTable.tsx
+++ src/components/FilteredTable.tsx
@@ -7,11 +7,11 @@ const FilteredTable = ({ data }) => {
 
     return (
         <div className="table-container my-11 bg-white overflow-y-scroll h-[400px]  ">
-            <div className='grid grid-cols-5 md:grid-cols-8 bg-[#F9F9FA] py-4 px-7 gap-3 text-[#343434] font-bold'>
+            <div className='grid grid-cols-4 md:grid-cols-8 bg-[#F9F9FA] py-4 px-7 gap-3 text-[#343434] font-bold'>
                 <div>S/N</div>
                 <div className='hidden md:block'>Surname</div>
                 <div>FirstName</div>
-                <div>Age</div>
+                <div className='hidden md:block'>Age</div>
                 <div className='hidden md:block'>Gender</div>
                 <div className='hidden md:block'>Level</div>
                 <div>State</div>
